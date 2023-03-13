18e8c43c8fa2a4a7260c750a6bee1ff802152552
Younes <younesesebbar089@gmail.com>
2023-03-13 16:21:43 +0100

    try recursive function examples

---
+++ Recursion.js
@@ -0,0 +1,146 @@
+//Reverse Solution
+
+function reverse(str) {
+  if (str.length <= 1) return str;
+  return reverse(str.slice(1)) + str[0];
+}
+//isPalindrome Solution
+
+function isPalindrome(str) {
+  if (str.length === 1) return true;
+  if (str.length === 2) return str[0] === str[1];
+  if (str[0] === str.slice(-1)) return isPalindrome(str.slice(1, -1));
+  return false;
+}
+//someRecursive Solution
+
+function someRecursive(array, callback) {
+  if (array.length === 0) return false;
+  if (callback(array[0])) return true;
+  return someRecursive(array.slice(1), callback);
+}
+//flatten Solution
+
+function flatten(arr) {
+  const flatArray = [];
+  if (arr.length === 0) return arr;
+  Array.from(arr).forEach((item) => {
+    if (Array.isArray(item)) {
+      flatArray.push(...flatten(item));
+    } else flatArray.push(item);
+  });
+  return flatArray;
+}
+
+function factoriel(num) {
+  if (num === 1) return 1;
+  return num * factoriel(num - 1);
+}
+
+function flatten(oldArr) {
+  var newArr = [];
+  for (var i = 0; i < oldArr.length; i++) {
+    if (Array.isArray(oldArr[i])) {
+      newArr = newArr.concat(flatten(oldArr[i]));
+    } else {
+      newArr.push(oldArr[i]);
+    }
+  }
+  return newArr;
+}
+
+function capitalizeWords(array) {
+  if (array.length === 1) {
+    return [array[0].toUpperCase()];
+  }
+  let res = capitalizeWords(array.slice(0, -1));
+  res.push(array.slice(array.length - 1)[0].toUpperCase());
+  return res;
+}
+//nestedEvenSum Solution
+function nestedEvenSum(obj) {
+  // add whatever parameters you deem necessary - good luck!
+  let sumTotal = 0;
+  function helper(input) {
+    for (let i = 0; i < Object.values(input).length; i++) {
+      let currentValue = Object.values(input)[i];
+      if (typeof currentValue === "object") helper(currentValue);
+      if (typeof currentValue === "number" && currentValue % 2 === 0)
+        sumTotal += currentValue;
+    }
+  }
+  helper(obj);
+  return sumTotal;
+}
+
+function nestedEvenSum(obj, sum = 0) {
+  for (var key in obj) {
+    if (typeof obj[key] === "object") {
+      sum += nestedEvenSum(obj[key]);
+    } else if (typeof obj[key] === "number" && obj[key] % 2 === 0) {
+      sum += obj[key];
+    }
+  }
+  return sum;
+}
+//capitalizeFire Solution
+
+function capitalizeFirst(array) {
+  if (array.length === 1) {
+    return [array[0][0].toUpperCase() + array[0].substr(1)];
+  }
+  const res = capitalizeFirst(array.slice(0, -1));
+  const string =
+    array.slice(array.length - 1)[0][0].toUpperCase() +
+    array.slice(array.length - 1)[0].substr(1);
+  res.push(string);
+  return res;
+}
+//stringifyNumbers Solution
+
+function stringifyNumbers(obj) {
+  var newObj = {};
+  for (var key in obj) {
+    if (typeof obj[key] === "number") {
+      newObj[key] = obj[key].toString();
+    } else if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+      newObj[key] = stringifyNumbers(obj[key]);
+    } else {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+}
+//collectStrings Solution: Helper Method Recursion Version
+
+function collectStrings(obj) {
+  var stringsArr = [];
+
+  function gatherStrings(o) {
+    for (var key in o) {
+      if (typeof o[key] === "string") {
+        stringsArr.push(o[key]);
+      } else if (typeof o[key] === "object") {
+        return gatherStrings(o[key]);
+      }
+    }
+  }
+
+  gatherStrings(obj);
+
+  return stringsArr;
+}
+//collectStrings Solution: Pure Recursion Version
+
+function collectStrings(obj) {
+  var stringsArr = [];
+  for (var key in obj) {
+    if (typeof obj[key] === "string") {
+      stringsArr.push(obj[key]);
+    } else if (typeof obj[key] === "object") {
+      stringsArr = stringsArr.concat(collectStrings(obj[key]));
+    }
+  }
+
+  return stringsArr;
+}
